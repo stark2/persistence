ee81b335d33472b0c4978bf7d8a138bb3b66d31e
David Stark <david.stark@protonmail.com>
2020-10-06 22:35:44 +0200

    Add JpaOfficerDAOTest

---
+++ src/test/java/com/oreilly/persistence/dao/OfficerRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.oreilly.persistence.dao;
+
+import com.oreilly.persistence.entities.Officer;
+import com.oreilly.persistence.entities.Rank;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+public class OfficerRepositoryTest {
+    @Autowired
+    private OfficerRepository repository;
+
+}
